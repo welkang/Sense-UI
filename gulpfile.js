1beab3fbe43066b1d1874a758c7ea446810444b1
WELKANG <welkang@qq.com>
2015-03-16 17:43:50 +0800

    初始化个人UI框架

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// 引入 gulp
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var gutil = require('gulp-util');
+
+gulp.task('less', function () {
+  gulp.src(['public/less/yui.less', 'public/less/style.less'])
+    .pipe(less())
+    .pipe(gulp.dest('public/css'))
+    .on('error', gutil.log);
+});
+
+gulp.task('watch', function(){
+	gulp.watch('**/*.less', ['less']);
+});
+
+// 默认任务
+gulp.task('default', ['less', 'watch']);
